4527038ae3c42ff363e4b893c36b054e46ee9436
wangxu <969545291@qq.com>
2016-11-12 17:47:45 +0800

    王旭

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,14 +7,12 @@
  * # MainCtrl
  * Controller of the homeApp
  */
-angular.module('homeApp')
-  .controller('a', function ($scope,$http) {
-    $http({
-		method:"get",
-		url:"http://www.somenote.cn:1602/list1"
-//		url:"http://localhost:9999/ajax/ajax414xinwen"
-	}).success(function (e){
-		$scope.data=e[0].content;
-	})
-			
+
+angular.module('yoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
   });
